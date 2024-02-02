6b7e23f3be94863d6ed6ada7e97d32400b4e075c
martin-genomics <martintembo.zm@gmail.com>
2024-02-02 07:26:15 +0200

    Colored requests for easy identification of http methods.

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { userRoutes } from './routes/api-routes';
 import { connectToDb } from './database/db';
+import { bgCyan, bgGreen, bgRed, bgYellow } from 'kleur';
+import { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
 
@@ -21,9 +23,24 @@ app.use(express.json());
 app.use(express.urlencoded( { extended: true }));
 app.use(expressFileupload());
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.method === 'POST') {
+      console.log(bgCyan(req.method), req.path);
+    } else if (req.method === 'GET') {
+      console.log(bgGreen(req.method), req.path);
+    } else if (req.method === 'DELETE') {
+      console.log(bgRed(req.method), req.path);
+    } else if (req.method === 'PATCH') {
+      console.log(bgYellow(req.method), req.path);
+    }
+    next();
+  });
+
 (async ()=> {
+    //This self-invoked function iniates a database connection
     await connectToDb();
 })();
+
 //Routes
 app.use('/api/v1.0', userRoutes);
 
