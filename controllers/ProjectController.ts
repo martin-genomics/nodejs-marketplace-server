c72b205f2de07542cfe92ac7e2d2a41cdca9e25f
martin-genomics <martintembo.zm@gmail.com>
2024-02-05 18:25:35 +0200

    Added multiple module functions but not yet implemented

---
+++ controllers/ProjectController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+
+
+export default class ProjectController {
+
+    static async index(req: Request, res: Response) {
+
+    }
+
+    static async createProject(req: Request, res: Response) {
+
+    }
+
+    static async addUser(req: Request, res: Response) {
+
+    }
+
+    static async deleteProject(req: Request, res: Response) {
+
+    }
+
+    static async activateProject(req: Request, res: Response) {
+
+    }
+    static async deactivateProject(req: Request, res: Response) {
+
+    }
+    static async updateProject(req: Request, res: Response) {
+
+
+    }
+
+
+
+}
